ca82f14b084028fd681cd21ebe7ee5a5cda1e3af
Andrii <andrey.skorochod@gmail.com>
2021-04-01 16:31:45 +0300

    ToHTML updated

--- src/utils/ToHtml.py
+++ src/utils/ToHtml.py
@@ -1,24 +1,24 @@
 class ToHtml
     _color_template = Template(
-        open("../templates/color.html").read()
+        open("../res/templates/color.html").read()
     )
 
     _index_template = Template(
-        open("../templates/index.html").read()
+        open("../res/templates/index.html").read()
     )
 
-    @StaticMethod
+    @staticmethod
     def color_to_html(color):
         return _color_template.substitute(rgb=color)
 
-    @StaticMethod
+    @staticmethod
     def create_content(*colors):
         result = ""
         for color in colors:
             result += color_to_html(color) + "\n"       
         return result
 
-    @StaticMethod
+    @staticmethod
     def create_index(colors_html):
         index_html = _index_template.substitute(content=colors_html) 
         return
